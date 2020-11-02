439bfd12cc44bd87071d43d351cffd54360305f8
Lesley Lai <lesley@lesleylai.info>
2020-11-01 18:17:34 -0700

    Drawing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,116 @@
-use winit::event::VirtualKeyCode;
+use futures::executor::block_on;
+use winit::event::{Event, VirtualKeyCode, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
 use winit_input_helper::WinitInputHelper;
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl State {
+    async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::Default,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                    shader_validation: true,
+                },
+                None,
+            )
+            .await
+            .unwrap();
+
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        Self {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            size,
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.size = new_size;
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+
+    fn render(&mut self) {
+        let frame = self
+            .swap_chain
+            .get_current_frame()
+            .expect("Timeout getting texture")
+            .output;
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("RENDER ENCODER"), // label in graphics debugger
+            });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &frame.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+    }
+}
 
 fn main() {
     let mut input = WinitInputHelper::new();
 
     let event_loop = EventLoop::new();
-    let _window = WindowBuilder::new()
+    let window = WindowBuilder::new()
         .with_title("WebGPU Experiments!")
         .build(&event_loop)
         .unwrap();
+
+    let mut state = block_on(State::new(&window));
 
     event_loop.run(move |event, _, control_flow| {
         if input.update(&event) {
@@ -18,6 +118,22 @@ fn main() {
                 *control_flow = ControlFlow::Exit;
                 return;
             }
+        }
+
+        match event {
+            Event::RedrawRequested(_) => {
+                state.render();
+            }
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::Resized(physical_size) => {
+                    state.resize(physical_size);
+                }
+                _ => {}
+            },
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            _ => {}
         }
     });
 }
